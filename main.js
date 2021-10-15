d46ea69f7a58c4d45b170f191ea6dd2dcbc8bcdd
SALIBA <florian.saliba@gmail.com>
2021-10-15 15:53:36 +0200

    fin de l'implémentation des fonctionnalités et des mediaqueries

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ if (window.location.pathname === "/index.html") {
     //Insérer une div de class "profile" pour chaque photographe sur la page "index.html"
 
     array.forEach((profil) => {
-      main.innerHTML += `<div class="profile" id="${profil.id}"></div>`;
+      main.innerHTML += `<div class="profile" id="${profil.id}" ></div>`;
     });
 
     // on récupère toutes les div de class "profile"
@@ -57,7 +57,7 @@ if (window.location.pathname === "/index.html") {
     for (let i = 0; i < divProfile.length; i++) {
       divProfile[
         i
-      ].innerHTML += `<form class="form-photographer" action="/photographer.html"><input type="hidden" name="id" value=${array[i].id}><img class="profile__img" src="" alt=""></img><h2 class="profile__name">exemple</h2><p class="profile__location">exemple</p><p class="profile__intro">exemple</p><p class="profile__price"></p><ul class="profile__skills"></ul></input></form>`;
+      ].innerHTML += `<form class="form-photographer" action="/photographer.html"><input id="hidden-input" type="hidden" name="id" value=${array[i].id}><img class="profile__img" src="" alt="${array[i].name}" role="img"></img><h2 class="profile__name">exemple</h2><p class="profile__location">exemple</p><p class="profile__intro">exemple</p><p class="profile__price"></p><ul class="profile__skills"></ul></input></form>`;
     }
 
     // Récupérer les éléments du DOM de la page principale
@@ -89,39 +89,66 @@ if (window.location.pathname === "/index.html") {
       });
     }
 
-    // Activer l'envoi du formulaire au click sur un profil
     const profilsForm = document.querySelectorAll(".profile");
+
+    // Activer l'envoi du formulaire au click sur un profil
     profilsForm.forEach((p) => {
       p.addEventListener("click", () => {
         p.firstElementChild.submit();
+      });
+      // Idem pour l'envoi du formulaire au clavier
+      p.addEventListener("focus", () => {
+        window.addEventListener("keydown", (e) => {
+          if (e.key === "Enter") {
+            p.firstElementChild.submit();
+          }
+        });
       });
     });
 
     // Faire apparaître le bouton "haut de page" au scroll
     document.addEventListener("scroll", () => {
       topButton.style.display = "block";
+      if (window.pageYOffset == 0) {
+        topButton.style.display = "none";
+      }
     });
 
     // permettre de revenir en haut de page
     topButton.addEventListener("click", () => {
       scrollTo(0, 0);
-      topButton.style.display = "none";
     });
   };
   render(profilsTable);
-  // Ajouter un listener sur les tags afin de filtrer les photographes
+
+  // Ajouter un listener au click sur les tags afin de filtrer les photographes
   const labels = document.getElementsByClassName("btn__speciality");
 
   for (let i = 0; i < labels.length; i++) {
     labels[i].addEventListener("click", (e) => {
       let labelName = e.target.textContent.split("#")[1].toLowerCase();
-      console.log(labelName);
+
       // Vérifier si le tag correspond a un des tags des photographes
 
       const filterProfil = profilsTable.filter((profil) => {
         if (profil.tags.includes(labelName)) return profil;
       });
       render(filterProfil);
+    });
+    // Idem pour activer le filtre au clavier
+    labels[i].addEventListener("focus", () => {
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+          let labelName = e.target.textContent.split("#")[1].toLowerCase();
+
+          // Vérifier si le tag correspond a un des tags des photographes
+
+          const filterProfil = profilsTable.filter((profil) => {
+            if (profil.tags.includes(labelName)) return profil;
+          });
+          render(filterProfil);
+        }
+      });
     });
   }
 }
@@ -148,11 +175,18 @@ if (window.location.pathname === "/photographer.html") {
 
   // Editer le HTML en fonction des données du photograph sélèctionné
 
-  profilPic.innerHTML = `<img class="profile__img" src="${selectedProfil[0].portrait} " alt=""></img>`;
-  presentation.innerHTML = `<h2 class="profile__name">${selectedProfil[0].name}</h2>
-    <p class="profile__location">${selectedProfil[0].city}</p>
-    <p class="profile__intro">${selectedProfil[0].tagline}</p>
-    <p class="profile__price">${selectedProfil[0].price}€/jour</p>`;
+  profilPic.innerHTML = `<img class="profile__img--photograph" src="${selectedProfil[0].portrait}" alt="${selectedProfil[0].name}" role="img"></img>`;
+  presentation.innerHTML = `<h1 class="profile__name--photograph">${selectedProfil[0].name}</h1>
+    <p class="profile__location--photograph">${selectedProfil[0].city}, ${selectedProfil[0].country}</p>
+    <p class="profile__intro--photograph">${selectedProfil[0].tagline}</p>
+    <ul class="profile__tagslist"></ul>`;
+
+  // Ajouter les tags du profil
+  const tagslist = document.getElementsByClassName("profile__tagslist")[0];
+  selectedProfil[0].tags.forEach((tag) => {
+    tagslist.innerHTML += `<li class="profile__tag--photograph">#${tag}</li>`;
+  });
+
   document.body.className = "second-body";
 
   // Filtrer les médias pour ne retenir que ceux du photographe ciblé
@@ -168,16 +202,14 @@ if (window.location.pathname === "/photographer.html") {
     array.forEach((media) => {
       // Si le media représente une image
       if (media.picture) {
-        picturesSection[0].innerHTML += `<div class="pictures__media"><img class = "pictures__pics" src="./pictures/${media.picture}"
-          ></img><div class="pictures__description"><p class="pictures__title">
-          ${media.title} 
-          </p><p class="pictures__number"> 
+        picturesSection[0].innerHTML += `<div class="pictures__media"><img class = "pictures__pics" src="./pictures/${media.picture}" alt="${media.title}" aria-label="Lilac breasted roller, closeup view" role="img" ></img><div class="pictures__description"><p class="pictures__title">
+          ${media.title}</p><div class="mini-container"><p class="pictures__number"> 
           ${media.likes} 
-          </p><i class="fas fa-heart"></i></div></div>`;
+          </p><i class="fas fa-heart" aria-label="Likes"></i></div></div></div>`;
       } else {
         // Le media représente une vidéo
-        picturesSection[0].innerHTML += `<div class="pictures__media"><video class = "pictures__pics" controls><source src="./pictures/${media.video}" type="video/mp4"></video>
-          <div class="pictures__description"><p class="pictures__title">${media.title}</p><p class="pictures__number">${media.likes}</p><i class="fas fa-heart"></i></div></div>`;
+        picturesSection[0].innerHTML += `<div class="pictures__media"><video class = "pictures__pics" alt="${media.title}" aria-label="Lilac breasted roller, closeup view" controls><source src="./pictures/${media.video}" type="video/mp4"></video>
+          <div class="pictures__description"><p class="pictures__title">${media.title}</p><div class="mini-container"><p class="pictures__number">${media.likes}</p><i class="fas fa-heart"></i></div></div></div>`;
       }
     });
     // Afficher le contenu de la balise "aside" en bas de page
@@ -200,9 +232,189 @@ if (window.location.pathname === "/photographer.html") {
         picturesNumber[i].textContent =
           parseInt(picturesNumber[i].textContent) + 1;
         totalOfLikes.textContent = parseInt(totalOfLikes.textContent) + 1;
-        console.log(totalOfLikes.textContent);
       });
     }
+    const lightboxFunction = () => {
+      // Faire apparaitre la lightbox au click de chaque image
+      let photographerPictures =
+        document.getElementsByClassName("pictures__pics");
+      const lightbox = document.getElementById("slider-section");
+      let ariaHidden = lightbox.getAttribute("aria-hidden");
+      let lightboxImage = document.getElementsByClassName("slider__img")[0];
+      const lightboxTitle = document.getElementsByClassName("slider__name")[0];
+
+      const lightboxCross = document.getElementById("slider__cross");
+      const openLightbox = () => {
+        lightbox.style.display = "flex";
+        divTotalLikes.style.display = "none";
+        ariaHidden = "false";
+      };
+
+      const closeLightbox = () => {
+        lightbox.style.display = "none";
+        divTotalLikes.style.display = "flex";
+      };
+      const nextBtn = document.getElementById("next-btn");
+      const previousBtn = document.getElementById("previous-btn");
+      const container = document.getElementsByClassName("slider__container")[0];
+
+      // ouverture de la lightbox
+
+      for (let i = 0; i < photographerPictures.length; i++) {
+        let picturesAndVideos = selectedMedias.map((media) => {
+          if (media.picture !== undefined) {
+            return media.picture;
+          } else {
+            return media.video;
+          }
+        });
+
+        // Fonction d'affichage de la lightbox
+        const renderLightbox = () => {
+          if (picturesAndVideos[i].split(".")[1] === "jpg") {
+            container.innerHTML = `<img class="slider__img" src="/pictures/${picturesAndVideos[i]}" alt="${selectedMedias[i].title}" aria-label="Lilac Breasted Roller" role="img"/><h3 class="slider__name">${selectedMedias[i].title}</h3>`;
+          } else {
+            container.innerHTML = `<video class = "slider__img" alt="${selectedMedias[i].title}" aria-label="Lilac Breasted Roller" controls><source src="./pictures/${picturesAndVideos[i]}" type="video/mp4"><h3 class="slider__name">${selectedMedias[i].title}</h3>`;
+          }
+        };
+
+        // Ouverture de la lightbox au clic
+        photographerPictures[i].addEventListener("click", () => {
+          openLightbox();
+          renderLightbox();
+          // Défilement vers la gauche
+          previousBtn.addEventListener("click", () => {
+            nextBtn.style.display = "block";
+            if (i === 0) {
+              renderLightbox();
+              previousBtn.style.display = "none";
+            } else {
+              i--;
+              renderLightbox();
+            }
+          });
+
+          // Défilement à droite
+          nextBtn.addEventListener("click", () => {
+            previousBtn.style.display = "block";
+            if (i === picturesAndVideos.length - 1) {
+              renderLightbox();
+              nextBtn.style.display = "none";
+            } else {
+              i++;
+              renderLightbox();
+            }
+          });
+
+          // Défilement au clavier
+
+          window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+              case "ArrowRight":
+                previousBtn.style.display = "block";
+                if (i === picturesAndVideos.length - 1) {
+                  renderLightbox();
+                  nextBtn.style.display = "none";
+                } else {
+                  i++;
+                  renderLightbox();
+                }
+                break;
+
+              case "ArrowLeft":
+                nextBtn.style.display = "block";
+                if (i === 0) {
+                  renderLightbox();
+                  previousBtn.style.display = "none";
+                } else {
+                  i--;
+                  renderLightbox();
+                }
+                break;
+
+              default:
+                return;
+            }
+          });
+        });
+
+        // Permettre l'ouverture de la ligthbox au clavier
+
+        photographerPictures[i].addEventListener("focus", () => {
+          window.addEventListener("keydown", (e) => {
+            if (e.key === "Enter" && ariaHidden === "true") {
+              openLightbox();
+              renderLightbox();
+              // Défilement vers la gauche
+              previousBtn.addEventListener("click", () => {
+                nextBtn.style.display = "block";
+                if (i === 0) {
+                  renderLightbox();
+                  previousBtn.style.display = "none";
+                } else {
+                  i--;
+                  renderLightbox();
+                }
+              });
+
+              // Défilement à droite
+              nextBtn.addEventListener("click", () => {
+                previousBtn.style.display = "block";
+                if (i === picturesAndVideos.length - 1) {
+                  renderLightbox();
+                  nextBtn.style.display = "none";
+                } else {
+                  i++;
+                  renderLightbox();
+                }
+              });
+
+              // Défilement au clavier
+
+              window.addEventListener("keydown", (e) => {
+                switch (e.key) {
+                  case "ArrowRight":
+                    previousBtn.style.display = "block";
+                    if (i === picturesAndVideos.length - 1) {
+                      renderLightbox();
+                      nextBtn.style.display = "none";
+                    } else {
+                      i++;
+                      renderLightbox();
+                    }
+                    break;
+
+                  case "ArrowLeft":
+                    nextBtn.style.display = "block";
+                    if (i === 0) {
+                      renderLightbox();
+                      previousBtn.style.display = "none";
+                    } else {
+                      i--;
+                      renderLightbox();
+                    }
+                    break;
+
+                  default:
+                    return;
+                }
+              });
+            }
+          });
+        });
+      }
+      // Fermeture de la lightbox au clic
+      lightboxCross.addEventListener("click", () => {
+        closeLightbox();
+      });
+      // Fermeture de la ligtbox au clavier
+      window.addEventListener("keydown", (e) => {
+        if (ariaHidden == "false" && e.key === "Escape") {
+          closeLightbox();
+        }
+      });
+    };
+    lightboxFunction();
   };
 
   renderMedias(selectedMedias);
@@ -222,7 +434,7 @@ if (window.location.pathname === "/photographer.html") {
   // Fonction trie des médias par Titre
   const sortByTitle = (mediaTable) => {
     mediaTable.sort((a, b) => {
-      return b.title - a.title >= 0 ? 1 : -1;
+      return b.title >= a.title ? -1 : 1;
     });
   };
 
@@ -243,98 +455,26 @@ if (window.location.pathname === "/photographer.html") {
     if (select.value === "Titre") {
       sortByTitle(selectedMedias);
       renderMedias(selectedMedias);
-      console.log(selectedMedias);
     } else if (select.value === "Date") {
       sortByDate(selectedMedias);
       renderMedias(selectedMedias);
-      console.log(selectedMedias);
     } else {
       sortByPopularity(selectedMedias);
       renderMedias(selectedMedias);
-      console.log(selectedMedias);
     }
   });
-
-  // Faire apparaitre la lightbox au click de chaque image
-  let photographerPictures = document.getElementsByClassName("pictures__pics");
-  const lightbox = document.getElementById("slider-section");
-  let lightboxImage = document.getElementsByClassName("slider__img")[0];
-
-  const lightboxCross = document.getElementById("slider__cross");
-  const openLightbox = () => {
-    lightbox.style.display = "flex";
-    divTotalLikes.style.display = "none";
-  };
-
-  const closeLightbox = () => {
-    lightbox.style.display = "none";
-    divTotalLikes.style.display = "flex";
-  };
-  const nextBtn = document.getElementById("next-btn");
-  const previousBtn = document.getElementById("previous-btn");
-  const container = document.getElementsByClassName("slider__container")[0];
-
-  // ouverture de la lightbox
-
-  const lightboxFunction = () => {
-    for (let i = 0; i < photographerPictures.length; i++) {
-      let picturesAndVideos = selectedMedias.map((media) => {
-        if (media.picture !== undefined) {
-          return media.picture;
-        } else {
-          return media.video;
-        }
-      });
-
-      photographerPictures[i].addEventListener("click", () => {
-        openLightbox();
-        const renderLightbox = () => {
-          if (picturesAndVideos[i].split(".")[1] === "jpg") {
-            container.innerHTML = `<img class="slider__img" src="/pictures/${picturesAndVideos[i]}" alt=""/>`;
-          } else {
-            container.innerHTML = `<video class = "slider__img"><source src="./pictures/${picturesAndVideos[i]}" type="video/mp4">`;
-          }
-        };
-        renderLightbox();
-
-        // Défilement à droite
-        nextBtn.addEventListener("click", () => {
-          previousBtn.style.display = "block";
-          if (i === picturesAndVideos.length - 1) {
-            renderLightbox();
-            nextBtn.style.display = "none";
-          } else {
-            i++;
-            renderLightbox();
-          }
-        });
-        // Défilement vers la gauche
-        previousBtn.addEventListener("click", () => {
-          nextBtn.style.display = "block";
-          if (i === 0) {
-            renderLightbox();
-            previousBtn.style.display = "none";
-          } else {
-            i--;
-            renderLightbox();
-          }
-        });
-      });
-    }
-
-    lightboxCross.addEventListener("click", () => {
-      closeLightbox();
-    });
-  };
-  lightboxFunction();
 
   // Ajouter 2 fonctions pour pouvoir ouvrir et fermer le formulaire
   const contactButton = document.getElementsByClassName("contact-me__btn")[0];
   const contactForm = document.getElementsByClassName("form-section")[0];
+  let modalClosed = contactForm.getAttribute("aria-hidden");
+  const contactTitle = document.getElementsByClassName("form__title")[0];
   const cross = document.getElementById("cross");
 
   const openModal = () => {
     contactForm.style.display = "flex";
+    contactTitle.textContent = `Contactez-moi ${selectedProfil[0].name}`;
+    modalClosed = "false";
   };
 
   const closeModal = () => {
@@ -344,8 +484,17 @@ if (window.location.pathname === "/photographer.html") {
   contactButton.addEventListener("click", () => {
     openModal();
   });
+
+  // Fermeture de la modale à la souris
   cross.addEventListener("click", () => {
     closeModal();
+  });
+
+  // Fermeture de la modale au clavier
+  window.addEventListener("keydown", (e) => {
+    if (modalClosed == "false" && e.key === "Escape") {
+      closeModal();
+    }
   });
 
   // Données d'entré du formulaire
